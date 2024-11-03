a22ea1e915000a18f9140a2a79d8004b1755a453
AengChoon <kevin1131@naver.com>
2024-11-04 00:47:40 +0900

    서버 코드 추가

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using ServerCore;
+
+namespace Server;
+
+internal static class Program
+{
+    private static readonly Listener Listener;
+
+    static Program()
+    {
+        string host = Dns.GetHostName();
+        IPHostEntry ipEntry = Dns.GetHostEntry(host);
+        IPAddress ipAddress = ipEntry.AddressList[0];
+        IPEndPoint endPoint = new IPEndPoint(ipAddress, 7777);
+        
+        Listener = new Listener(endPoint, OnAcceptHandler);
+    }
+
+    private static void OnAcceptHandler(Socket clientSocket)
+    {
+        try
+        {
+            byte[] receiveBuffer = new byte[1024];
+            int receivedBytes = clientSocket.Receive(receiveBuffer);
+            string receivedString = Encoding.UTF8.GetString(receiveBuffer, 0, receivedBytes);
+            Console.WriteLine($"[From Client] {receivedString}");
+
+            byte[] sendBuffer = "Welcome to MMORPG Server!"u8.ToArray();
+            clientSocket.Send(sendBuffer);
+
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+    
+    public static void Main(string[] args)
+    {
+        Listener.Listen();
+        Console.WriteLine("Listening...");
+
+        while (true)
+        {
+            
+        }
+    }
+}
